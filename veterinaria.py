bdd04851d72e7d3d208114d2d9ecd4aca3562574
eduardomendezgomez <kevin.mendezgomez.2005@gmail.com>
2024-08-17 10:27:40 -0600

    añadiendo y creando nueva metodologia

--- veterinaria.py
+++ veterinaria.py
@@ -8,7 +8,7 @@ class Perro:
         self.dueno = dueno 
         self.estado = "No atendido"
         self.tipo = self.Dtipo()
-
+#creando una funcion que defina conforme al peso si es mayor que 10 es  perro grande, sino, es perro pequeño
     def Dtipo(self):
         if self.peso > 10:
             return "Perro Grande"
@@ -18,26 +18,28 @@ class Perro:
     def RegistrarPerro(self):
         estado = "Atendido"
         return estado
-    def imprmirDatos(self):
-        print(f"Nombre del perro: {self.nombre}")
-        print(f"Edad del perro: {self.edad}")
-        print(f"Peso del perro: {self.peso}")
-        print(f"Raza del perro: {self.raza}")
-        print(f"Color del perro: {self.color}")
-        print(f"Dueño del perro: {self.dueno}")
-        print(f"Tipo de perro: {self.tipo}")
-        print(f"Estado del perro: {self.estado}")
+
+
+def IngresarPerro():
+    nombre = input("Nombre del perrito: ")
+    edad = int(input("Edad del perrito: "))
+    peso = float(input("peso del perrito en kg: "))
+    raza = input("Raza del perrito: ")
+    color = input("Color del perrito: ")
+    dueno = input("Nombre del dueño: ")
+    print("**************************************************")
+    nuevoPerro = Perro(nombre,edad,peso,raza,color,dueno)
+    return nuevoPerro
+def imprmirDatos(perro):
+        print(f"Nombre del perro: {perro.nombre}")
+        print(f"Edad del perro: {perro.edad}")
+        print(f"Peso del perro: {perro.peso} kg")
+        print(f"Raza del perro: {perro.raza}")
+        print(f"Color del perro: {perro.color}")
+        print(f"Dueño del perro: {perro.dueno}")
+        print(f"Tipo de perro: {perro.tipo}")
+        print(f"Estado del perro: {perro.estado}")
         print("****************************************************")
 
-
-
-nombre = input("Nombre del perrito: ")
-edad = int(input("Edad del perrito: "))
-peso = float(input("peso del perrito: "))
-raza = input("Raza del perrito: ")
-color = input("Color del perrito: ")
-dueno = ("Nombre del dueño: ")
-print("**************************************************")
-
-nuevoPerro = Perro(nombre,edad,peso,raza,color,dueno)
-nuevoPerro.imprmirDatos()
+perro = IngresarPerro()
+imprmirDatos(perro)
